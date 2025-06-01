ad6bdeca3aa6f673d3f2b0c9525f3e5d64b568e8
Sumit <sumeetyadav1642@gmail.com>
2025-06-02 04:48:16 +0530

    Added the full crud support for Posts

---
+++ routes/MainPosts.js
@@ -0,0 +1,109 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { verifyAuth } from "../middlewares/MainPostsMiddlewares.js";
+
+const prisma = new PrismaClient();
+export const postsRoutes = express.Router();
+
+postsRoutes.get("/posts", async (req, res) => {
+	const posts = await prisma.post.findMany({
+		orderBy: { createdAt: "desc" },
+		include: { author: { select: { name: true } } },
+	});
+
+	return res.status(200).json({
+		status: "success",
+		message: "All posts fetched",
+		data: posts,
+	});
+});
+
+postsRoutes.get("/my-posts", verifyAuth, async (req, res) => {
+	const posts = await prisma.post.findMany({
+		where: { authorId: req.user.user_ID },
+		orderBy: { createdAt: "desc" },
+	});
+
+	return res.status(200).json({
+		status: "success",
+		message: "My posts fetched",
+		data: posts,
+	});
+});
+
+postsRoutes.post("/posts", verifyAuth, async (req, res) => {
+	const { title, content } = req.body;
+
+	if (!title || !content) {
+		return res.status(400).json({
+			status: "error",
+			message: "No fields should be empty",
+			code: 400,
+		});
+	}
+
+	const newPost = await prisma.post.create({
+		data: {
+			title,
+			content,
+			authorId: req.user.user_ID,
+		},
+	});
+
+	return res.status(201).json({
+		status: "success",
+		message: "Post created successfully",
+		data: newPost,
+	});
+});
+
+postsRoutes.put("/posts/:id", verifyAuth, async (req, res) => {
+	const { id } = req.params;
+	const { title, content } = req.body;
+
+	const post = await prisma.post.findUnique({
+		where: { id: parseInt(id) },
+	});
+
+	if (!post || post.authorId !== req.user.user_ID) {
+		return res.status(403).json({
+			status: "error",
+			message: "Not authorized or post not found",
+			code: 403,
+		});
+	}
+
+	const updatedPost = await prisma.post.update({
+		where: { id: parseInt(id) },
+		data: { title, content },
+	});
+
+	return res.status(200).json({
+		status: "success",
+		message: "Post updated",
+		data: updatedPost,
+	});
+});
+
+postsRoutes.delete("/posts/:id", verifyAuth, async (req, res) => {
+	const { id } = req.params;
+
+	const post = await prisma.post.findUnique({
+		where: { id: parseInt(id) },
+	});
+
+	if (!post || post.authorId !== req.user.user_ID) {
+		return res.status(403).json({
+			status: "error",
+			message: "Not authorized or post not found",
+			code: 403,
+		});
+	}
+
+	await prisma.post.delete({ where: { id: parseInt(id) } });
+
+	return res.status(200).json({
+		status: "success",
+		message: "Post deleted",
+	});
+});
